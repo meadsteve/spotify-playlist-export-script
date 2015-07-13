05683d0ebf4aeeaa01cd192f130a32a9d93e58c0
steve.brazier <meadsteve@gmail.com>
2015-07-13 18:27:40 +0100

    Add shell script to grab sarah's playlists also add output to lists/ new file:   get-wedding-lists.sh new file:   lists/wedding-1.txt new file:   lists/wedding-2-aisle.txt new file:   lists/wedding-3-classicaldrinks.txt new file:   lists/wedding-4-foodolder.txt new file:   lists/wedding-5-foodnewer.txt new file:   lists/wedding-6-betweenbandsets.txt new file:   lists/wedding-7-postbandpredj.txt new file:   lists/wedding-8-postdjchill.txt

---
+++ get-wedding-lists.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+node spot-grab.js spotify:user:sarah_mottram:playlist:6UsZW2QCEn5lXZcMTSOMh4 > lists/wedding-1.txt
+node spot-grab.js spotify:user:sarah_mottram:playlist:41l421G8akUEdokNJLqoqv > lists/wedding-2-aisle.txt
+node spot-grab.js spotify:user:sarah_mottram:playlist:00NRl1V16fcr18uaISUnQc > lists/wedding-3-classicaldrinks.txt
+node spot-grab.js spotify:user:sarah_mottram:playlist:5pmN4vWyYnnaP4xRN4hWMW > lists/wedding-4-foodolder.txt
+node spot-grab.js spotify:user:sarah_mottram:playlist:3UaWQE0tvJTAXAKZDOjx8V > lists/wedding-5-foodnewer.txt
+node spot-grab.js spotify:user:sarah_mottram:playlist:10Oa5I5ZEEFmRGTX87uHg5 > lists/wedding-6-betweenbandsets.txt
+node spot-grab.js spotify:user:sarah_mottram:playlist:44N84DJ6xlcnj2wlqYlrL2 > lists/wedding-7-postbandpredj.txt
+node spot-grab.js spotify:user:sarah_mottram:playlist:1AEgDAqNuMv8SK6mqkCT29 > lists/wedding-8-postdjchill.txt
